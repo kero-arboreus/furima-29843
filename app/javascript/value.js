af352d8a1d764fdf2022f04d71aef24f48fad234
kero-arboreus <0yuka.odachi0@gmail.com>
2020-09-22 16:57:42 +0900

    販売手数料と利益を非同期で表示

---
+++ app/javascript/value.js
@@ -0,0 +1,13 @@
+function value(){
+  const itemPrice = document.getElementById("item-price");
+  itemPrice.addEventListener("keyup", (e) => {
+    const taxPrice = itemPrice.value * 0.1;
+    const addTaxPrice = document.getElementById("add-tax-price");
+    addTaxPrice.innerHTML = `${taxPrice}`;
+    const profitCalc = itemPrice.value - taxPrice;
+    const profit = document.getElementById("profit");
+    profit.innerHTML = `${profitCalc}`;
+  });
+};
+
+window.addEventListener("load", value);
