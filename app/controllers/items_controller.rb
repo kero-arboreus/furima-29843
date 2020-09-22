9192bf46624563c1cee44da5ab0f731dbc3384f3
kero-arboreus <0yuka.odachi0@gmail.com>
2020-09-22 16:02:13 +0900

    バリデーションを設定してエラーメッセージを表示

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -9,7 +9,14 @@ class ItemsController < ApplicationController
   end
 
   def create
-    Item.create(item_params)
+    @item = Item.new(item_params)
+    
+    if @item.valid?
+      @item.save
+      return redirect_to root_path
+    else
+      render "new"
+    end
   end
 
   private
--- app/models/item.rb
+++ app/models/item.rb
@@ -8,4 +8,21 @@ class Item < ApplicationRecord
   belongs_to_active_hash :shipment_region
   belongs_to_active_hash :shipdate
 
+
+  with_options presence: true do
+    validates :image
+    validates :product_name
+    validates :description
+    validates :value, format: {with: /\A[0-9]+\z/, message: "Half-width number."}
+  end
+
+  validates :value, numericality: { greater_than_or_equal_to: 300, less_than_or_equal_to: 9999999, message: "Out of setting range"}
+
+  with_options numericality: { other_than: 1, message: "Select" } do
+   validates :category_id
+   validates :state_id
+   validates :freight_id
+   validates :shipment_region_id
+   validates :shipdate_id
+  end
 end
