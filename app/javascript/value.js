8f97867cc4cfeb0edad08a184dad29604421763a
kero-arboreus <0yuka.odachi0@gmail.com>
2020-09-22 17:53:36 +0900

    turbolinksをコメントアウト

--- app/javascript/value.js
+++ app/javascript/value.js
@@ -3,10 +3,10 @@ function value(){
   itemPrice.addEventListener("keyup", (e) => {
     const taxPrice = itemPrice.value * 0.1;
     const addTaxPrice = document.getElementById("add-tax-price");
-    addTaxPrice.innerHTML = `${taxPrice}`;
+     addTaxPrice.innerHTML = `${taxPrice}`;
     const profitCalc = itemPrice.value - taxPrice;
     const profit = document.getElementById("profit");
-    profit.innerHTML = `${profitCalc}`;
+     profit.innerHTML = `${profitCalc}`;
   });
 };
 
