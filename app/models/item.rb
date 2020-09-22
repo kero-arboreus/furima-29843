f09199c82de3e1f9ead601ceb2bfb5bb9cef6378
kero-arboreus <0yuka.odachi0@gmail.com>
2020-09-22 14:42:46 +0900

    発送元地域、発送日数のActiveHash作成

--- app/models/item.rb
+++ app/models/item.rb
@@ -4,4 +4,7 @@ class Item < ApplicationRecord
   belongs_to_active_hash :category
   belongs_to_active_hash :state
   belongs_to_active_hash :freight
+  belongs_to_active_hash :shipment_region
+  belongs_to_active_hash :shipdate
+
 end
