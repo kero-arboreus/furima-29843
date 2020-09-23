543ea8fba05db6555fbb3a0ca09197828d126e54
kero-arboreus <0yuka.odachi0@gmail.com>
2020-09-23 14:31:02 +0900

    Rubocopを実行

--- app/models/item.rb
+++ app/models/item.rb
@@ -8,21 +8,20 @@ class Item < ApplicationRecord
   belongs_to_active_hash :shipment_region
   belongs_to_active_hash :shipdate
 
-
   with_options presence: true do
     validates :image
     validates :product_name
     validates :description
-    validates :value, format: {with: /\A[0-9]+\z/, message: "Half-width number"}
+    validates :value, format: { with: /\A[0-9]+\z/, message: 'Half-width number' }
   end
 
-  with_options numericality: { other_than: 1, message: "Select" } do
-   validates :category_id
-   validates :state_id
-   validates :freight_id
-   validates :shipment_region_id
-   validates :shipdate_id
+  with_options numericality: { other_than: 1, message: 'Select' } do
+    validates :category_id
+    validates :state_id
+    validates :freight_id
+    validates :shipment_region_id
+    validates :shipdate_id
   end
 
-  validates :value, numericality: { greater_than_or_equal_to: 300, less_than_or_equal_to: 9999999, message: "Out of setting range"}
+  validates :value, numericality: { greater_than_or_equal_to: 300, less_than_or_equal_to: 9_999_999, message: 'Out of setting range' }
 end
