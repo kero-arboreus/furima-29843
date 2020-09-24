7ebb6e5fd50f5f8b72f87fb0e4b70d11358a78a2
kero-arboreus <0yuka.odachi0@gmail.com>
2020-09-24 16:09:43 +0900

    エラーハンドリングを修正

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,6 +1,6 @@
 class ItemsController < ApplicationController
   before_action :authenticate_user!, only: [:new, :create]
-  before_action :set_item, only: [:show, :edit]
+  before_action :set_item, only: [:show, :edit, :destroy]
 
   def index
     @items = Item.all.order('created_at DESC')
@@ -38,9 +38,12 @@ class ItemsController < ApplicationController
   end
 
   def destroy
-    @item = Item.find(params[:id])
-    @item.destroy if current_user.id == @item.user_id
+   if current_user.id == @item.user_id
+    @item.destroy
     redirect_to root_path
+   else
+    render 'show'
+   end
   end
 
   private
