418c0d155ca35881108fcfe881e9711d4eb3809c
kero-arboreus <0yuka.odachi0@gmail.com>
2020-09-25 19:57:23 +0900

    pay_itemメソッドを修正

--- app/controllers/managements_controller.rb
+++ app/controllers/managements_controller.rb
@@ -7,19 +7,20 @@ class ManagementsController < ApplicationController
   def create
     @item = Item.find(params[:item_id])
     @management = @item.management.new(management_params) 
+    @management.save
   end
 
   private
 
   def management_params
-    params.permit(:token)
+    params.require(:buyer).permit(:token, :postal_code, :shipment_region_id, :city, :address, :phone_num).merge(user_id: current_user.id, item_id: params[:item_id])
   end
 
   def pay_item
     Payjp.api_key = ENV["PAYJP_SECRET_KEY"]
     Payjp::Charge.create(
-      amount: management_params[:value],
-      card: order_params[:token],
+      amount: @item[:value],
+      card: management_params[:token],
       currency:'jpy'
     )
   end
