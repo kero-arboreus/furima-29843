cb61d74babb059c2e3ae50061771ff40a78a22c4
kero-arboreus <0yuka.odachi0@gmail.com>
2020-10-02 16:45:06 +0900

    カード情報登録用のルーティング、コントローラー

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,10 @@
 Rails.application.routes.draw do
+  get 'cards/new'
   get 'users/show'
   devise_for :users
   root to: 'items#index'
   resources :users, only: [:show, :edit, :update]
+  resources :cards, only: [:new, :create]
   resources :items do
     resources :managements, only: [:index, :create]
   end
