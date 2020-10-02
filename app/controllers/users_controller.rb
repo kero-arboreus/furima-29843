36344557d27e0a315aa293f69e95a26b79d34123
kero-arboreus <0yuka.odachi0@gmail.com>
2020-10-02 16:38:32 +0900

    ユーザー登録情報の変更画面を実装

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,9 +1,27 @@
 class UsersController < ApplicationController
-  
+
   def show
   end
 
   def edit
+  
   end
 
+  def update
+    current_user.update(user_params)
+
+    if current_user.valid?
+      sign_in(current_user, :bypass => true)
+      redirect_to root_path
+    else
+      render 'edit'
+    end
+  end
+
+  private
+
+  def user_params
+    params.require(:user).permit(:password, :nickname, :email, :first_name, :last_name, :first_name_kana, :last_name_kana, :birth)
+  end
 end
+
