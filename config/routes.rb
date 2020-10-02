506b975f9e2c683750e1afc3dac02115709e335f
kero-arboreus <0yuka.odachi0@gmail.com>
2020-10-02 15:28:14 +0900

    user-showのマークアップ

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,8 @@
 Rails.application.routes.draw do
+  get 'users/show'
   devise_for :users
   root to: 'items#index'
-  resources :users, only: [:show, :update]
+  resources :users, only: [:show, :edit, :update]
   resources :items do
     resources :managements, only: [:index, :create]
   end
